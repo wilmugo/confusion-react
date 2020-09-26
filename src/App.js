51e442e125364d51047a8b344fbe8b8015178bdf
Wilfredo Murillo <wilmugo@gmail.com>
2020-09-26 10:41:09 -0400

    Componentes parte 1: Se agrego componente Menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponent'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
 
     </div>
   );
