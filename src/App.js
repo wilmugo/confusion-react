88f9916d069702ba317cb328aaf555425d7e32c6
Wilfredo Murillo <wilmugo@gmail.com>
2020-09-26 12:19:36 -0400

    Componentes parte 2: cambiando a cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent'
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/"> 
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
 
-    </div>
-  );
+  }
+  
+  render() {
+    return (
+      <div className="App">
+
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/"> 
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
