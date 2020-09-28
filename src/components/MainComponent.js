37a5879ba05ac43a12d75ccffc06a430b6f43039
Wilfredo Murillo <wilmugo@gmail.com>
2020-09-27 22:35:09 -0400

    Assignment 2 addign about page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import  Menu  from "./MenuComponent";
 import  DishDetail from "./DishdetailComponent";
 import { DISHES } from '../shared/dishes';
@@ -51,6 +52,7 @@ class Main extends Component {
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
